c299c066af18adc2accb1a4a896433891cdc4f06
Maxim Matyunin <krabovm@gmail.com>
2023-04-29 15:04:50 +0000

    feat: add ya metrika and google verification

--- projects/blog/server/src/Main.hs
+++ projects/blog/server/src/Main.hs
@@ -95,9 +95,33 @@ wrapContent (title, body) =
         H.head $ do
             H.title (H.toHtml title)
             H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1"
+            H.meta ! A.name "google-site-verification" ! A.content "bI9akRHitJ4Ate9XY-zDTXozQGMV0k1YjcCcXPxIFtI"
             H.link ! A.href (fromString bootstrapCssLocation) ! A.rel "stylesheet" ! integrity (fromString bootstrapCssSha) ! crossorigin "anonymous"
             H.link ! A.href "/assets/css/prism.css" ! A.rel "stylesheet"
             H.link ! A.href "/assets/css/article.css" ! A.rel "stylesheet"
+            H.script $ do
+                (fromString . unlines)
+                    [ "const metrikaCounter = 90878380;"
+                    , "(function (m, e, t, r, i, k, a) {"
+                    , "m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };"
+                    , "m[i].l = 1 * new Date();"
+                    , "for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }"
+                    , "k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)"
+                    , "})"
+                    , "(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');"
+                    , ""
+                    , "ym(metrikaCounter, 'init', {"
+                    , "defer: true,"
+                    , "clickmap: true,"
+                    , "trackLinks: true,"
+                    , "accurateTrackBounce: true,"
+                    , "webvisor: true"
+                    , "});"
+                    ]
+            H.noscript $ do
+                H.div $ do
+                    H.img ! A.src "https://mc.yandex.ru/watch/90878380" ! A.style "position:absolute; left:-9999px;" ! A.alt ""
+
         H.body $ do
             H.div ! A.class_ "d-flex flex-column" ! A.style "min-height: 100vh" $ do
                 H.div $ do
